d5b46809da78b94110b1376fd7ac65dd25ec501f
Shiva Katukoori <shiva7k7@gmail.com>
2024-08-17 15:21:24 -0400

    local caching implemented

--- src/App.jsx
+++ src/App.jsx
@@ -6,38 +6,48 @@ import Footer from "./components/Footer.jsx";
 function App() {
     const [showModal, setShowModal] = useState(false)
     const [data, setData] = useState(null)
-    const [loading, setLoading] = useState(false)
-
-    const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
 
     function handleToggleModal() {
-        setShowModal(!showModal )
+        setShowModal(!showModal)
     }
 
     useEffect(() => {
         async function fetchNASA() {
-            const url = 'https://api.nasa.gov/planetary/apod'+`?api_key=${NASA_KEY}`
+            const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+            const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+            const today = (new Date()).toDateString();
+            const localKey = `NASA-${today}`
+
+            if (localStorage.getItem(localKey)) {
+                const apiData = JSON.parse(localStorage.getItem(localKey))
+                setData(apiData)
+                console.log('fetched from cache')
+                return
+            }
+            localStorage.clear()
             try {
                 const response = await fetch(url)
                 const apiData = await response.json()
+                localStorage.setItem(localKey, JSON.stringify(apiData))
                 setData(apiData)
-                console.log('data', apiData)
-            }catch (error) {
+                console.log('data fetched from api', apiData)
+            } catch (error) {
                 console.log(error.message)
             }
         }
-        fetchNASA()
+
+        fetchNASA().then()
     }, [])
 
     return (
         <>
-            {data? (<Main data={data}/>): (
+            {data ? (<Main data={data}/>) : (
                 <div className="loadingState">
                     <i className="fa-solid fa-gear"></i>
                 </div>
             )}
-            {showModal && (<Sidebar data={data} handleToggleModal={handleToggleModal}/>)}
-                {( data && (<Footer data={data} handleToggleModal={handleToggleModal}/>))}
+                {showModal && (<Sidebar data={data} handleToggleModal={handleToggleModal}/>)}
+                {(data && (<Footer data={data} handleToggleModal={handleToggleModal}/>))}
         </>
     )
 }
